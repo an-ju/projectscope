a6b1bbcfdb79e3c76998c1ba9b758ccc728dd954
an-ju <an_ju@berkeley.edu>
2017-10-24 10:09:25 -0700

    [feat] Update admin control for production deployment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,16 @@ class ApplicationController < ActionController::Base
   def passthru
     if params[:passwd].eql? ENV['ADMIN_PASSWORD']
       user_id = params[:id]
+      if User.find_by(uid: user_id).nil?
+        if user_id.eql?('uadmin')
+          user = User.create!(provider_username: 'Admin', uid: user_id, email: "#{user_id}@example.com",
+                              provider: "developer", role: User::ADMIN, password: Devise.friendly_token[0,20],
+                              preferred_metrics: [], preferred_projects: [])
+          Whitelist.create!(username: user.provider_username)
+        else
+          raise ActionController::RoutingError.new('Not Found')
+        end
+      end
       sign_in_and_redirect User.find_by(uid: user_id)
     else
       raise ActionController::RoutingError.new('Not Found')
