7e99286b61cb73795dca410f57ad081ba225bce6
an-ju <an_ju@berkeley.edu>
2017-10-27 15:51:44 -0700

    [fix] Fix error caused by new CC version.

--- app/assets/javascripts/draw.js
+++ app/assets/javascripts/draw.js
@@ -63,9 +63,17 @@ function drawMetricCharts(containerID, metric_sample) {
     } else if (JSONStr['chartType'] === 'commit_message') {
         commit_message(containerID, JSONStr.data);
     } else if (JSONStr['chartType'] === 'code_climate_v2') {
-        index_score(containerID, metric_sample.score.toPrecision(3), 0.0, 4.0);
+        if (metric_sample.score > 0) {
+            index_score(containerID, metric_sample.score.toPrecision(3), 0.0, 4.0);
+        } else {
+            code_climate_badge(containerID, JSONStr);
+        }
     } else if (JSONStr['chartType'] === 'test_coverage_v2') {
-        index_score(containerID, metric_sample.score.toPrecision(3), 0.0, 100.0);
+        if (metric_sample.score > 0) {
+            index_score(containerID, metric_sample.score.toPrecision(3), 0.0, 100.0);
+        } else {
+            test_coverage_badge(containerID, JSONStr);
+        }
     }
     else {
         error_message(containerID, metric_sample);
