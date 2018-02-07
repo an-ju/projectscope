00c610c1563290f860921c4102905d67dff74b47
Xiao Fu <fxdawn@berkeley.edu>
2018-02-06 23:47:06 -0800

    Fix task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
-  has_many: inedge, class_name: "Task", foreign_key: "outedge_id"
-  belongs_to: outedge, class_name: "Task", foreign_key: "inedge_id"
+  has_many :inedge, class_name: "Task", foreign_key: "outedge_id"
+  belongs_to :outedge, class_name: "Task", foreign_key: "inedge_id"
 
 end
