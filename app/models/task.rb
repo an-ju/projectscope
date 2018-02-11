196ee889c2a4861197187c076eec25cbcb2bb2a3
Xiao Fu <fxdawn@berkeley.edu>
2018-02-10 16:33:30 -0800

    Add model method to abstract the graph

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,4 +7,20 @@ class Task < ActiveRecord::Base
 
   has_many :parentstask, through: :parentedge, source: :parenttask
   has_many :parentedge, foreign_key: :parentedge_id, class_name: "Taskedge"
+
+  def self.abstract_graph start_task
+    children = Taskedge.find_children(start_task)
+    graph = Hash.new
+    visited = children
+    graph[start_task] = Array.new(children)
+    until children.empty?
+      newnode = children.shift
+      newchildren = Taskedge.find_children newnode
+      graph[newnode] = Array.new(newchildren)
+      newchildren.delete_if{|child| visited.include? child}
+      visited.concat(newchildren)
+      children.concat(newchildren)
+    end
+    JSON.generate(graph)
+  end
 end
