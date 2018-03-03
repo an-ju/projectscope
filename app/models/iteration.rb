1d903ee74297e1e574578c1a2609b0e9fc82f5f6
Xiao Fu <fxdawn@berkeley.edu>
2018-03-03 10:35:01 -0800

    Show the tree with vacancy

--- app/models/iteration.rb
+++ app/models/iteration.rb
@@ -28,7 +28,7 @@ class Iteration < ActiveRecord::Base
     children = graph[root]
     visited = Array.new(children)
     visited.append(root)
-    children.each{ |child| graphlevel[child] = level + 1}
+    children.each{ |child| graphlevel[child] = level + 1 }
     until children.empty?
       newnode = children.shift
       newchildren = graph[newnode]
@@ -37,20 +37,26 @@ class Iteration < ActiveRecord::Base
       visited.concat(newchildren)
       children.concat(newchildren)
     end
-    Iteration.level_up graphlevel
+    Iteration.level_up graphlevel,graph
   end
 
   # return the 2-d lists where the key is stored inside the level slot
-  def self.level_up graphlevel
+  def self.level_up graphlevel, graph
     rank = Array.new
+    pos = Hash.new
     graphlevel.each do |key, value|
       if rank[value].nil?
         rank[value] = Array.new
       end
       rank[value].append key
+      if (graph[key].length) > 0
+        rank[value].concat([nil] * ((graph[key].length) - 1))
+      end
+      pos[key] = rank[value].length
     end
     rank
   end
+
 
   # Find out the height and length of the graph
   def self.max_level_elem ranklist
