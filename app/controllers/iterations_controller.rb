0e6731f3bdddf28b3fa2c94baee562ee431fa76f
Xiao Fu <fxdawn@berkeley.edu>
2018-05-16 17:18:47 -0700

    Change iteration for Travis

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -42,31 +42,11 @@ class IterationsController < ApplicationController
   # POST /iterations
   # POST /iterations.json
   def create
-    @iteration = Iteration.new(iteration_params)
-
-    respond_to do |format|
-      if @iteration.save
-        format.html { redirect_to @iteration, notice: 'Iteration was successfully created.' }
-        format.json { render :show, status: :created, location: @iteration }
-      else
-        format.html { render :new }
-        format.json { render json: @iteration.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /iterations/1
   # PATCH/PUT /iterations/1.json
   def update
-    respond_to do |format|
-      if @iteration.update(iteration_params)
-        format.html { redirect_to @iteration, notice: 'Iteration was successfully updated.' }
-        format.json { render :show, status: :ok, location: @iteration }
-      else
-        format.html { render :edit }
-        format.json { render json: @iteration.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /iterations/1
