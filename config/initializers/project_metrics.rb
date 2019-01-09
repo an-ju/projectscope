0d385f12a00a40a6d6b87477baeb167e369f3b76
an-ju <an_ju@berkeley.edu>
2019-01-08 17:33:05 -0800

    updated metrics to the latest version.

--- config/initializers/project_metrics.rb
+++ config/initializers/project_metrics.rb
@@ -1,16 +1,18 @@
 ProjectMetrics.configure do
   add_metric :project_metric_code_climate
-  add_metric :project_metric_story_overall
   add_metric :project_metric_test_coverage
-  add_metric :project_metric_pull_requests
   add_metric :project_metric_travis_ci
-  add_metric :project_metric_github_branch
-  add_metric :project_metric_github_files
-  add_metric :project_metric_github_flow
-  add_metric :project_metric_github_use
-  add_metric :project_metric_point_distribution
-  add_metric :project_metric_commit_message
   add_metric :project_metric_heroku_status
+  add_metric :project_metric_point_distribution
+
+  add_metric :project_metric_github_flow
+  add_metric :project_metric_pull_requests
+  add_metric :project_metric_github_branch
+  add_metric :project_metric_github_use
+  add_metric :project_metric_story_overall
+
+  add_metric :project_metric_github_files
+  add_metric :project_metric_commit_message
 
   add_hierarchy metric: [%I[test_coverage code_climate travis_ci heroku_status point_distribution],
                          %I[github_flow pull_requests github_branch github_use story_overall]]
